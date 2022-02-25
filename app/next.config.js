25f678e659631a003522b79a9c2b5dab66491c8b
tribeo <tribeok55@gmail.com>
2022-02-25 09:13:17 +0700

    redirect `/` to `/column`

--- app/next.config.js
+++ app/next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['www-stg.koov.io', 'secure.gravatar.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/column',
+        permanent: true,
+      },
+    ]
+  },
 };
