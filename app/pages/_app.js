3d98420b52190caccbfbeb09d70e5ea538ab9f93
tribeo <tribeok55@gmail.com>
2022-02-28 16:38:03 +0700

    change favicon for koov

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,15 @@
 import "../styles/index.css";
-import "../styles/icomoon.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
