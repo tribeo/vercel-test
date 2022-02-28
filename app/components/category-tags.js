28d05e19c56403e73c60e09a1d72024b5c9da532
tribeo <tribeok55@gmail.com>
2022-02-28 16:39:52 +0700

    add link for each category tag

--- app/components/category-tags.js
+++ app/components/category-tags.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./category-tags.module.css";
 
 export default function CategoryTags({ categories }) {
@@ -6,13 +7,14 @@ export default function CategoryTags({ categories }) {
       {categories.length > 0 ? (
         <div className={`${styles["post-categories"]} category-tags-container`}>
           {categories.map((category, index) => (
-            <div
-              key={index}
-              className={`${styles["post-category-item"]} ${
-                styles[category.node.slug]
-              }`}
-              dangerouslySetInnerHTML={{ __html: category.node.name }}
-            ></div>
+            <Link href={`/column/${category.node.slug}`} key={index}>
+              <a
+                className={`${styles["post-category-item"]} ${
+                  styles[category.node.slug]
+                }`}
+                dangerouslySetInnerHTML={{ __html: category.node.name }}
+              ></a>
+            </Link>
           ))}
         </div>
       ) : null}
